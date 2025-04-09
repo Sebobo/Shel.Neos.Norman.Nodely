96648b51de26ce3b7fe483e78a7961351c461b20
Sebastian Helzle <sebastian@helzle.it>
2025-04-09 22:18:12 +0200

    Motivate user when failing at login

--- Resources/Public/Login.js
+++ Resources/Public/Login.js
@@ -3,9 +3,43 @@ document.addEventListener('DOMContentLoaded', () => {
     const nodelyInjector = document.getElementById('nodelyInjector');
     const theNormanSVG = nodelyInjector.querySelector('svg');
     const eyeOffsetDegrees = nodelyInjector.dataset.eyeOffsetDegrees || 60;
+    const errorTooltip = document.querySelector('.neos-tooltip-error');
 
+    const messages = [
+        'Do you even know your own credentials?',
+        'Seriously? That’s your best attempt?',
+        'Come on, focus! You can do better.',
+        'Are you even trying?',
+        'This is getting embarrassing. Try harder.',
+        'Wrong again. Maybe double-check this time?',
+        'It’s like you don’t even know yourself.',
+        'Let’s take a moment to reflect on this failure.',
+        'Incorrect. Perhaps a tutorial would help?',
+        'At this point, I’m questioning your commitment.',
+        'Did you already try turning it off and on again?',
+        'I’m starting to think you might need a password manager.',
+        'This is not a game. You can’t keep guessing forever.',
+        'I’m not sure if you’re joking or just really bad at this.',
+        'This is getting sad. Maybe ask for help?',
+        'I’m not mad, just disappointed.',
+        'You know, there are people who can help you with this.',
+        'This is like watching someone try to parallel park.',
+        'I’m starting to think you might need a new password.',
+        'You’re making this way harder than it needs to be.',
+        'This is like watching someone try to solve a Rubik’s cube blindfolded.',
+    ];
+
+    // Go away bad logo, go!
     logoWrapper.innerHTML = '';
+
+    // Norman shall be the one logo!
     logoWrapper.appendChild(theNormanSVG)
+
+    // If the user is to bad at entering their password, we need to make them understand the error of their ways.
+    if (errorTooltip) {
+        errorTooltip.querySelector('.neos-tooltip-inner').innerText = messages[Math.floor(Math.random() * messages.length)];
+        logoWrapper.appendChild(errorTooltip);
+    }
 
     document.addEventListener("mousemove", (e) => {
         movePupils(e);
